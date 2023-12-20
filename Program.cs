5cd3b6a735df7da1f77356bece3017c181447436
Efraim <efraim.alima@gmail.com>
2023-12-20 16:41:28 -0300

    fix: resolvendo problemas propostos pelos desafios do bootcamp

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@
 Smartphone nokia = new Nokia(numero: "", modelo: "", imei: "", memoria: 3);
 nokia.Ligar();
 nokia.ReceberLigacao();
-nokia.InstalarAplicativo(nomeApp: "Nokia joquinho");
+nokia.InstalarAplicativo(nomeApp: "Nokia joguinho");
 
 Smartphone iphone = new Iphone(numero: "", modelo: "", imei: "", memoria: 3);
 iphone.Ligar();
 iphone.ReceberLigacao();
-iphone.InstalarAplicativo(nomeApp: "iPhone joquinho");
+iphone.InstalarAplicativo(nomeApp: "iPhone joguinho");
